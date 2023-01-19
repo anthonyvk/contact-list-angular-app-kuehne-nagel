1b4dd1da9a5886317e9023f4755e0f84757119a9
Anthony Vinay Kumar S <anthonyvinaykumar1992@gmail.com>
2023-01-20 01:32:49 +0200

    Add UI changes to Search and List contacts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,107 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {DomSanitizer} from "@angular/platform-browser";
+// import console from "console";
+
+const BASEURL = 'http://localhost:8080/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'contact-list-angular-app-kuehne-nagel';
+  contacts: any;
+  currentIndex = -1;
+
+  page = 1;
+  count = 0;
+  pageSize = 10;
+  pageSizes = [3, 6, 9];
+
+  nameSearchValue = '';
+
+  constructor(
+    private httpClient: HttpClient,
+    private sanitizer:DomSanitizer)
+  {}
+
+  ngOnInit(): void {
+    this.retrieveContacts();
+  }
+
+  getAll(params: any): Observable<any> {
+    return this.httpClient.get(BASEURL + '/getAllContacts', {params});
+  }
+
+  getAllByName(params: any): Observable<any> {
+    return this.httpClient.get(BASEURL + '/find', {params});
+  }
+
+  getRequestParams(searchTitle: string, page: number, pageSize: number, nameSearchValue: string): any {
+    // tslint:disable-next-line:prefer-const
+    let params: any = {};
+    if (searchTitle) {
+      params[`title`] = searchTitle;
+    }
+    if (page) {
+      params[`page`] = page - 1;
+    }
+    if (pageSize) {
+      params[`size`] = pageSize;
+    }
+    if(nameSearchValue) {
+      params[`searchValue`] = nameSearchValue;
+    }
+    return params;
+  }
+
+  retrieveContacts(): void {
+    const params = this.getRequestParams(this.title, this.page, this.pageSize, this.nameSearchValue);
+
+    this.getAll(params)
+      .subscribe(
+        response => {
+          const {contactList, totalContacts} = response;
+          console.log("contacts = " + contactList)
+          this.contacts = contactList;
+          this.count = totalContacts;
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  searchContactsByName(): void {
+    const params = this.getRequestParams(this.title, this.page, this.pageSize, this.nameSearchValue);
+
+    this.getAllByName(params)
+      .subscribe(
+        response => {
+          const {contactList, totalContacts} = response;
+          console.log("contacts = " + contactList)
+          this.contacts = contactList;
+          this.count = totalContacts;
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  handlePageChange(event: number): void {
+    this.page = event;
+    this.retrieveContacts();
+  }
+
+  handlePageSizeChange(event: any): void {
+    this.pageSize = event.target.value;
+    this.page = 1;
+    this.retrieveContacts();
+  }
+
+  sanitize(url:string){
+    return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
 }
